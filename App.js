8e886b2b1a3688006e3d5714f1e9a93a6df01e71
Claudio Junior <cso.junior1996@gmail.com>
2019-01-16 00:11:53 -0200

    Aula: DebugJS & Reactotron

--- App.js
+++ App.js
@@ -1,42 +1,13 @@
+import "./src/config/ReactotronConfig";
+
 import React, { Component } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
+console.tron.log("Hello World");
+
 export default class App extends Component {
-  state = {
-    text: "",
-    counter: 0
-  };
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ text: "Hello World" });
-    }, 3000);
-  }
-
-  static getDerivedStateFromProps(nextProps, prevState) {
-    return { text: nextProps.text };
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    return nextState.counter < 5;
-  }
-
-  componentDidUpdate(prevProps, prevState) {}
-
-  componentWillUnmount() {}
-
-  handleAddCounter = () => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
-
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>{this.state.text}</Text>
-        <Text>{this.state.counter}</Text>
-        <Button onPress={this.handleAddCounter} title="Add" />
-      </View>
-    );
+    return <View style={styles.container} />;
   }
 }
 
